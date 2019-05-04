b50850b4f44f94828113ac3234a581b8eaedb118
claes-magnus <cmbernson@gmail.com>
2019-05-04 14:39:32 +0200

    Replace type Point: array -> struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #[derive(Debug)]
 struct Point {
-    pos: [i32; 2]
+    x: i32,
+    y: i32
 }
-
 
 fn main() {
     let result = get_coordinates(1, 1, 3, 3);
@@ -30,7 +30,7 @@ fn get_coordinates(x1: i32, y1: i32, x2: i32, y2: i32) -> Vec<Point> {
     let mut error:i32 = dx - dy;
     let mut current_x:i32 = x1;
     let mut current_y:i32 = y1;
-    coordinates.push(Point {pos: [current_x, current_y]});
+    coordinates.push(Point { x: current_x, y: current_y });
     while current_x != x2 && current_y != y2 {
         
         let error2:i32 = 2 * error;
@@ -41,7 +41,7 @@ fn get_coordinates(x1: i32, y1: i32, x2: i32, y2: i32) -> Vec<Point> {
             error += dx;
             current_y += sy;
         }
-        coordinates.push(Point {pos: [current_x, current_y]});
+        coordinates.push(Point { x: current_x, y: current_y });
     }
     coordinates
 }
