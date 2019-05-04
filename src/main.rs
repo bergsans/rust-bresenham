421d2c3346a14ab1ccb87c7307af1c122ee024b3
claes-magnus <cmbernson@gmail.com>
2019-05-04 14:31:28 +0200

    Change algorithm to include end-point, and add test

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ struct Point {
 
 
 fn main() {
-    let result = get_coordinates(1, 1, 10, 5);
+    let result = get_coordinates(1, 1, 3, 3);
     println!("{:?}", result);
 }
 
-fn get_coordinates(x1: i32, y1: i32, x2: i32, y2: i32) -> Vec<Point> { //-> std::vec::Vec<i32>
-    let mut coordinates = vec![];  //Vec::new();
+fn get_coordinates(x1: i32, y1: i32, x2: i32, y2: i32) -> Vec<Point> {
+    let mut coordinates = vec![];
     let dx:i32 = i32::abs(x2 - x1);
     let dy:i32 = i32::abs(y2 - y1);
     let sx:i32 = {
@@ -30,8 +30,9 @@ fn get_coordinates(x1: i32, y1: i32, x2: i32, y2: i32) -> Vec<Point> { //-> std:
     let mut error:i32 = dx - dy;
     let mut current_x:i32 = x1;
     let mut current_y:i32 = y1;
+    coordinates.push(Point {pos: [current_x, current_y]});
     while current_x != x2 && current_y != y2 {
-        coordinates.push(Point {pos: [current_x, current_y]});
+        
         let error2:i32 = 2 * error;
         if error2 > i32::abs(dy) {
             error -= dy;
@@ -40,7 +41,17 @@ fn get_coordinates(x1: i32, y1: i32, x2: i32, y2: i32) -> Vec<Point> { //-> std:
             error += dx;
             current_y += sy;
         }
+        coordinates.push(Point {pos: [current_x, current_y]});
     }
-    //println!("{:?}", coordinates);
     coordinates
+}
+
+#[cfg(test)]
+mod tests {
+   use super::*;
+ 
+    #[test]
+    fn test_add() {
+        assert!(get_coordinates(1, 1, 3, 3), [Point { pos: [1, 1] }, Point { pos: [1, 2] }, Point { pos: [2, 2] }, Point { pos: [2, 3] }]);
+    }
 }
